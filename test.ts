97b17d9b77790f136d19b283b51c3029eafe0e61
ssande-fwd <ssande@forwardedu.com>
2025-05-30 14:15:37 -0400

    remove temperature

--- test.ts
+++ test.ts
@@ -1,31 +1,5 @@
-// It's too hectic to run all the tests simultaneously.
-// Add temperature simulator. Remove temperature and add pH simulator. Remove pH and add lightbulb and button simulators.
-
-// temperature tests
-// temperature(): number
-// isPastThreshold(threshold: number, direction: ThresholdDirection): boolean
-console.log("temperature: " + sensors.temperature1.temperature())
-basic.forever(function () {
-    if (typeof sensors.temperature1.temperature() === "number") {
-        if (
-            sensors.temperature1.isPastThreshold(
-                25,
-                sensors.ThresholdDirection.Over
-            )
-        ) {
-            console.log(sensors.temperature1.temperature() + " is over 25°C")
-        }
-        if (
-            sensors.temperature1.isPastThreshold(
-                25,
-                sensors.ThresholdDirection.Under
-            )
-        ) {
-            console.log(sensors.temperature1.temperature() + " is under 25°C")
-        }
-        basic.pause(1000)
-    }
-})
+// It's hectic to run all the tests simultaneously.
+// Add pH simulator. Remove pH and add lightbulb and button (float) simulators.
 
 // pH tests
 // ph(): number
