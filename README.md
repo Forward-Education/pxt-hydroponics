c92c1fbd6d63055c0c8f45995d622f1af7d37834
ssande-fwd <ssande@forwardedu.com>
2025-06-08 22:37:29 -0400

    extension approval related changes

--- README.md
+++ README.md
@@ -1,15 +1,22 @@
-# pxt-hydroponics
+# pxt-fwd-hydroponics
 
-Hydroponics Kit, by Forward Education
+Smart Hydroponics Kit, by Forward Education.
 
-Find us at [forwardedu.com](https://forwardedu.com/) and [learn.forwardedu.com](https://learn.forwardedu.com/). Learn more about the Hydroponics Kit on the [product page](https://forwardedu.com/products/smart-hydroponics-kit).
+Find us at [forwardedu.com](https://forwardedu.com/) and [learn.forwardedu.com](https://learn.forwardedu.com/). Learn more about the Smart Hydroponics Kit on the [product page](https://forwardedu.com/products/smart-hydroponics-kit).
+
+### ~ reminder
+
+![works with micro:bit V2 only image](/static/v2/v2-only.png)
+
+These blocks require the [micro:bit V2](/device/v2). If you use them with a V1 micro:bit you will see the 927 error code on the screen.
+
+### ~
 
 ## Example Usage
 
 Our learning systems are designed to simplify teaching coding and computer science for educators at all experience levels.
-Our Hydroponics Kit can be used on its own or joined with other kits to access our wider library of sensors, motors, lights, and buttons.
-Check out our lesson catalogue for curriculum-aligned lessons, including educator notes.
-A sample of coding with the Hydroponics Kit can be seen below.
+Our Smart Hydroponics Kit can be used on its own or joined with other kits to access our wider library of sensors, motors, lights, and buttons.
+Check out our libraries of [lessons](https://learn.forwardedu.com/lesson-library), [projects](https://learn.forwardedu.com/projects/), and [tutorials](https://learn.forwardedu.com/tutorials/). A sample of coding with the Smart Hydroponics Kit can be seen below.
 
 The light controller, pH sensor, float sensor, and water pump help create and maintain ideal growing conditions.
 
--- test.ts
+++ test.ts
@@ -5,41 +5,41 @@
 // ph(): number
 // isPastThreshold(threshold: number, direction: ThresholdDirection): boolean
 // calibrate(standard1: number, reading1: number, standard2: number, reading2: number): void
-console.log("ph: " + sensors.ph1.ph())
-basic.forever(function () {
-    if (typeof sensors.ph1.ph() === "number") {
-        if (sensors.ph1.isPastThreshold(7, sensors.ThresholdDirection.Over)) {
-            console.log(sensors.ph1.ph() + " is over 7")
-        }
-        if (sensors.ph1.isPastThreshold(7, sensors.ThresholdDirection.Under)) {
-            console.log(sensors.ph1.ph() + " is under 7")
-        }
-        basic.pause(1000)
-    }
-})
+console.log("ph: " + fwdSensors.ph1.ph())
 input.onButtonPressed(Button.A, function () {
-    sensors.ph1.calibrate(4, 5, 7, 8)
+    if (fwdSensors.ph1.isPastThreshold(7, fwdEnums.OverUnder.Over)) {
+        console.log(fwdSensors.ph1.ph() + " is over 7")
+    }
+    if (fwdSensors.ph1.isPastThreshold(7, fwdEnums.OverUnder.Under)) {
+        console.log(fwdSensors.ph1.ph() + " is under 7")
+    }
+    basic.pause(1000)
+})
+input.onButtonPressed(Button.B, function () {
+    fwdSensors.ph1.calibrate(4, 5, 7, 8)
 })
 
 // lights tests
 // setBrightness(value: number)
-input.onLogoEvent(TouchButtonEvent.Pressed, function () {
+basic.forever(() => {
     console.log("Test Start")
     console.log(
         "The brightness is changing every second for 6 seconds, but the Jacdac simulator only shows on / off."
     )
     basic.pause(1000)
-    lights.lights1.setBrightness(100)
+    fwdLights.lights1.setBrightness(100)
+    console.log("Light on? " + fwdLights.lights1.isOn())
     basic.pause(1000)
-    lights.lights1.setBrightness(75)
+    fwdLights.lights1.setBrightness(75)
     basic.pause(1000)
-    lights.lights1.setBrightness(50)
+    fwdLights.lights1.setBrightness(50)
     basic.pause(1000)
-    lights.lights1.setBrightness(25)
+    fwdLights.lights1.setBrightness(25)
     basic.pause(1000)
-    lights.lights1.setBrightness(10)
+    fwdLights.lights1.setBrightness(10)
     basic.pause(1000)
-    lights.lights1.setBrightness(0)
+    fwdLights.lights1.setBrightness(0)
+    console.log("Light on? " + fwdLights.lights1.isOn())
     console.log("Test End")
 })
 
@@ -47,15 +47,39 @@ input.onLogoEvent(TouchButtonEvent.Pressed, function () {
 // onFloatChange(state: FloatState, handler: () => void)
 // floatStateConditional(state: FloatState): boolean
 // floatState(): string
-sensors.float1.onFloatChange(sensors.FloatState.raised, () => {
+fwdSensors.float1.onFloatChange(fwdEnums.RaisedLowered.Raised, () => {
     console.log("Event: raised")
-    if (sensors.float1.floatStateConditional(sensors.FloatState.raised)) {
-        console.log("State: " + sensors.float1.floatState())
-    }
 })
-sensors.float1.onFloatChange(sensors.FloatState.lowered, () => {
+fwdSensors.float1.onFloatChange(fwdEnums.RaisedLowered.Lowered, () => {
     console.log("Event: lowered")
-    if (sensors.float1.floatStateConditional(sensors.FloatState.lowered)) {
-        console.log("State: " + sensors.float1.floatState())
+})
+basic.forever(() => {
+    if (
+        fwdSensors.float1.floatStateConditional(fwdEnums.RaisedLowered.Raised)
+    ) {
+        console.log("State: " + fwdSensors.float1.floatState())
     }
+    if (
+        fwdSensors.float1.floatStateConditional(fwdEnums.RaisedLowered.Lowered)
+    ) {
+        console.log("State: " + fwdSensors.float1.floatState())
+    }
+    basic.pause(1000)
+})
+
+// pump tests
+// isOn(): boolean
+// setOn(on: boolean): void
+// timedRun(duration: number): void
+// setOn() is used in timedRun, so it is covered by timedRun() test
+basic.forever(() => {
+    console.log("Pump on? " + fwdMotors.pump.isOn())
+    console.log("Run pump for 3s.")
+    fwdMotors.pump.timedRun(3)
+    basic.pause(1000)
+    console.log("1s... Pump on? " + fwdMotors.pump.isOn())
+    basic.pause(1000)
+    console.log("2s... Pump on? " + fwdMotors.pump.isOn())
+    basic.pause(1000)
+    console.log("3s... Pump on? " + fwdMotors.pump.isOn())
 })
